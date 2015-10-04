79906ede5a4371de206aa7f9dde6d3b2a67618f5
Kaan Çelen <bkaancelen@gmail.com>
2015-10-04 16:16:27 +0300

    changelog

--- changelog.php
+++ changelog.php
@@ -5,6 +5,8 @@
 		<li>Talep, Poliçe istek, Poliçe İptal ve Poliçelerim ekranları yenilendi</li>
 		<li>Poliçe istek, Poliçe İptal ve Poliçeler artık 'ay/yıl' bazında görüntülenebiliyor.</li>
 		<li>Mutabakat sistemi geçici süreliğine kapatıldı.</li>
+		<li>Mesaj simgesi değişti.</li>
+		<li>Belirli sürede teklifleri tamamlanmayan taleplere uyarı simgesi eklendi.</li>
 	</ul>
 	<br><br><br>
 	<p>Uygulamadaki değişikliklerin yansıması için <br> bulunduğunuz sayfayı en az 1 defa <b>ctrl+f5</b> tuşları ile yenileyiniz.</p>
