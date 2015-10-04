04065d3c753ef79a8cb1d1f2a8b2ddb1d0593dba
Kaan Çelen <bkaancelen@gmail.com>
2015-10-04 12:59:58 +0300

    changelog

--- changelog.php
+++ changelog.php
@@ -1,8 +1,10 @@
 <div class="changelog">
-	<h3>Positive yeni sürüm 21 Eylül 2015</h3>
+	<h3>Positive yeni sürüm 4 Ekim 2015</h3>
 	<ul>
-		<li>Teknikçiler için poliçe sahibi acenta bilgilerini düzenleme özelliği eklendi.</li>
-		<li>Mutabakat sistemi eklendi.</li>
+		<li>Poliçe İptaller ayrı bir ekrana taşındı.</li>
+		<li>Talep, Poliçe istek, Poliçe İptal ve Poliçelerim ekranları yenilendi</li>
+		<li>Poliçe istek, Poliçe İptal ve Poliçeler artık 'ay/yıl' bazında görüntülenebiliyor.</li>
+		<li>Mutabakat sistemi geçici süreliğine kapatıldı.</li>
 	</ul>
 	<br><br><br>
 	<p>Uygulamadaki değişikliklerin yansıması için <br> bulunduğunuz sayfayı en az 1 defa <b>ctrl+f5</b> tuşları ile yenileyiniz.</p>
