21bac422916c8c16bf2ef77df7918fc918f7f073
Kaan Çelen <bkaancelen@gmail.com>
2015-10-11 21:37:07 +0300

    changelog

--- changelog.php
+++ changelog.php
@@ -1,8 +1,8 @@
 <div class="changelog">
-	<h3>Positive yeni sürüm 8 Ekim 2015</h3>
+	<h3>Positive yeni sürüm 12 Ekim 2015</h3>
 	<ul>
-		<li>Geç kalan taleplerin üste çıkarılması deaktif edildi.(Teknikçiler için)</li>
-		<li>Talep ekranına acente filtresi eklendi.(Teknikçiler için)</li>
+		<li>Talep ekranına kademeli yükleme özelliği getirildi.(Teknikçi)</li>
+		<li>Mutabakat ekranları gözden geçirildi ve düzenlendi(Acente, Finans)</li>
 	</ul>
 	<br><br><br>
 	<p>Uygulamadaki değişikliklerin yansıması için <br> bulunduğunuz sayfayı en az 1 defa <b>ctrl+f5</b> tuşları ile yenileyiniz.</p>
