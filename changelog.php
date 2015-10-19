cbc24f5082fb75788af6a2caa784aebd46b6eecd
Kaan Çelen <bkaancelen@gmail.com>
2015-10-19 20:36:37 +0300

    changelog

--- changelog.php
+++ changelog.php
@@ -1,7 +1,8 @@
 <div class="changelog">
-	<h3>Positive yeni sürüm 18 Ekim 2015</h3>
+	<h3>Positive yeni sürüm 20 Ekim 2015</h3>
 	<ul>
-		<li>Kademeli yükleme tekrar getirildi ve sayfa revize edildi.(Teknikçi)</li>
+		<li>Talep ekranından iptal ve tamamlanmışlar kaldırıldı.(Teknikçi)</li>
+		<li>Sadece Acenteler için ekranda gözüken talep süresi 6 güne çıkarıldı.(Acente)</li>
 	</ul>
 	<br><br><br>
 	<p>Uygulamadaki değişikliklerin yansıması için <br> bulunduğunuz sayfayı en az 1 defa <b>ctrl+f5</b> tuşları ile yenileyiniz.</p>
