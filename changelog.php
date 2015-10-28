47da7bb735b7ff6c59f3db942145805ac3def7f3
Kaan Çelen <bkaancelen@gmail.com>
2015-10-28 18:57:24 +0300

    changelog

--- changelog.php
+++ changelog.php
@@ -1,8 +1,8 @@
 <div class="changelog">
-	<h3>Positive yeni sürüm 21 Ekim 2015</h3>
+	<h3>Positive yeni sürüm 1 Kasım 2015</h3>
 	<ul>
-		<li>Talep ekranına parçalı yükleme getirildi.(Teknikçi)</li>
-		<li>Poliçeler ekranına acente filtresi eklendi.(Teknikçi)</li>
+		<li>Acente -> Üst Acente -> Bağlı Acente ilişkisi eklendi.</li>
+		<li>Mütabakat ekranı düzenlendi.</li>
 	</ul>
 	<br><br><br>
 	<p>Uygulamadaki değişikliklerin yansıması için <br> bulunduğunuz sayfayı en az 1 defa <b>ctrl+f5</b> tuşları ile yenileyiniz.</p>
