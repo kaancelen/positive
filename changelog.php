ad8aa7fe6ad00cc50b350fe48d4df9bf4c4327e1
Kaan Çelen <bkaancelen@gmail.com>
2015-11-07 09:25:43 +0300

    changelog

--- changelog.php
+++ changelog.php
@@ -1,7 +1,7 @@
 <div class="changelog">
-	<h3>Positive yeni sürüm 1 Kasım 2015</h3>
+	<h3>Positive yeni sürüm 8 Kasım 2015</h3>
 	<ul>
-		<li>Acente -> Üst Acente -> Bağlı Acente ilişkisi eklendi.</li>
+		<li>Virgüllü komisyon oranları eklendi</li>
 		<li>Mütabakat ekranı düzenlendi.</li>
 	</ul>
 	<br><br><br>
