2ce1aab8798ccde2559bea315f795c2c0ca42cf6
Kaan Çelen <bkaancelen@gmail.com>
2015-11-22 19:58:07 +0200

    changelog

--- changelog.php
+++ changelog.php
@@ -3,9 +3,11 @@
 	<br><br>
 	<p>Uygulamadaki değişikliklerin yansıması için <br> bulunduğunuz sayfayı en az 1 defa <b>ctrl+f5</b> tuşları ile yenileyiniz.</p>
 	<br><br>
-	<h3>Positive yeni sürüm 8 Kasım 2015</h3>
+	<h3>Positive yeni sürüm 23 Kasım 2015</h3>
 	<ul>
-		<li>Virgüllü komisyon oranları eklendi</li>
-		<li>Mütabakat ekranı düzenlendi.</li>
+		<li>Talep, poliçe sayfalarına filtre eklendi.(Teknikçi)</li>
+		<li>Üretim taleplerine <b>marka kodu</b> bilgisi eklendi.</li>
+		<li>Talep bilgilerini düzenleyebilme eklendi.</li>
+		<li>Prim bilgilerini düzenleyebilme eklendi.(Teknikçi)</li>
 	</ul>
 </div>
